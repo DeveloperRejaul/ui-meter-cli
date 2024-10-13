938aa5768f0b13ad4fac2f2f11ed56df54641819
Rezaul Karim <rezaul@orangetoolz.com>
2024-10-13 18:52:12 +0600

    radio btn edit

--- src/content.ts
+++ src/content.ts
@@ -370,44 +370,50 @@ export function radioContent() {
 import { createContext, useContext, useState } from 'react';
 import { Pressable, Text, TextStyle, View, ViewProps, ViewStyle } from 'react-native';
 
-interface IRadioProps {
-    size?: number;
-    inActiveColor?: string;
-    activeColor?: string;
-    inValidColor?: string;
-    label?: string;
-    isInvalid?: boolean;
-    isDisabled?: boolean;
-    onChange?: (value: string) => void;
-    value: string;
-    borderStyle?: ViewStyle
-    borderActiveStyle?: ViewStyle
-    borderInactiveStyle?: ViewStyle
-    ballStyle?: ViewStyle
-    ballActiveStyle?: ViewStyle
-    ballInactiveStyle?: ViewStyle
-    containerStyle?: ViewStyle
-    containerActiveStyle?: ViewStyle
-    containerInactiveStyle?: ViewStyle
-    labelStyle?: TextStyle
-    labelActiveStyle?: TextStyle
-    labelInactiveStyle?: TextStyle
-}
-interface IContextRadioGroup{
-    setActiveValue: React.Dispatch<React.SetStateAction<string>>;
-    activeValue: string
-    onChange?: (value: string) => void;
+export interface IRadioProps {
+  size?: number;
+  inActiveColor?: string;
+  activeColor?: string;
+  inValidColor?: string;
+  label?: string;
+  isInvalid?: boolean;
+  isDisabled?: boolean;
+  onChange?: (value: string) => void;
+  value: string;
+  borderStyle?: ViewStyle
+  borderActiveStyle?: ViewStyle
+  borderInactiveStyle?: ViewStyle
+  ballStyle?: ViewStyle
+  ballActiveStyle?: ViewStyle
+  ballInactiveStyle?: ViewStyle
+  containerStyle?: ViewStyle
+  containerActiveStyle?: ViewStyle
+  containerInactiveStyle?: ViewStyle
+  labelStyle?: TextStyle
+  labelActiveStyle?: TextStyle
+  labelInactiveStyle?: TextStyle
+  text?: string;
+  gap?: number
+  textStyle?: TextStyle
+  textActiveStyle?: TextStyle
+  textInactiveStyle?: TextStyle
+ }
+export interface IContextRadioGroup{
+  setActiveValue: React.Dispatch<React.SetStateAction<string>>;
+  activeValue: string
+  onChange?: (value: string) => void;
 }
 
 export interface IRadioGroup extends ViewProps{
-    onChange?: (value: string) => void;
-    children: React.ReactNode
+  onChange?: (value: string) => void;
+  children: React.ReactNode;
+  defaultActive?: string;
 }
 
 export const Context = createContext<IContextRadioGroup>({} as IContextRadioGroup);
 
 export function RadioGroup(props: IRadioGroup) {
-  const [activeValue, setActiveValue] = useState('');
+  const [activeValue, setActiveValue] = useState(props.defaultActive ? props.defaultActive : '');
   const { onChange, children } = props || {};
   return (
     <Context.Provider value={{ activeValue, setActiveValue, onChange }}>
@@ -418,12 +424,13 @@ export function RadioGroup(props: IRadioGroup) {
   );
 }
 
+
 export function Radio(props:IRadioProps) {
   const {
     size = 25,
-    inActiveColor = '#000',
-    activeColor = '#EC8305',
-    inValidColor = '#87A2FF',
+    inActiveColor = color.dark,
+    activeColor = color.active,
+    inValidColor = color.primary,
     borderStyle,
     ballStyle,
     ballActiveStyle,
@@ -441,11 +448,16 @@ export function Radio(props:IRadioProps) {
     isInvalid,
     label,
     onChange: radioOnChange,
+    text,
+    gap = 0.6,
+    textActiveStyle,
+    textInactiveStyle,
+    textStyle,
   } = props;
 
   const { activeValue, setActiveValue, onChange } = useContext(Context);
 
-  const BOX_SIZE = size * 0.6;
+  const BOX_SIZE = size * gap;
   const BOX_RADIUS = BOX_SIZE / 2;
   const CONTAINER_RADIUS = size * 0.5;
   const BORDER_WIDTH = size * 0.1;
@@ -457,7 +469,7 @@ export function Radio(props:IRadioProps) {
       style={[{
         justifyContent: 'center',
         alignItems: 'center',
-        columnGap: 5,
+        columnGap: 20,
         opacity: isDisabled ? DISABLE_OPACITY : 1,
         ...containerStyle,
       }, isActive ? containerActiveStyle : containerInactiveStyle]}
@@ -488,12 +500,27 @@ export function Radio(props:IRadioProps) {
             height: BOX_SIZE,
             borderRadius: BOX_RADIUS,
             backgroundColor: isActive ? activeColor : 'transparent',
+            justifyContent: 'center',
+            alignItems: 'center',
             ...ballStyle,
           }, isActive ? ballActiveStyle : ballInactiveStyle]}
-        />
+        >
+          {text && (
+          <Text
+            style={[{
+              fontSize: 10,
+              color: color.dark,
+              fontWeight: '800',
+              ...textStyle,
+            }, isActive ? textActiveStyle : textInactiveStyle]}
+          >
+            {text}
+          </Text>
+          )}
+        </View>
       </Pressable>
       {label && (
-      <Text style={[{ fontWeight: 'bold', fontSize: 15, color: '#000', ...labelStyle }, isActive ? labelActiveStyle : labelInactiveStyle]}>
+      <Text style={[{ fontWeight: 'bold', fontSize: 15, color: color.dark, ...labelStyle }, isActive ? labelActiveStyle : labelInactiveStyle]}>
         {label}
       </Text>
       )}
