ddf9dfc5ca455c74456241fe5bb00efbec3bfdb1
Rezaul Karim <rezaul@orangetoolz.com>
2024-10-16 23:43:01 +0600

    add input component

--- example/app/index.tsx
+++ example/app/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import { store } from '@/rtk/store';
-import Switch from '@/components/switch';
+import Input from '@/components/input';
 
 export default function Index() {
   return (
     <Provider store={store}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Switch
-          size={50}
-          onChange={(value) => {
-            console.log(value);
-          }}
-        />
+        <Input />
       </View>
     </Provider>
   );
---
+++ example/components/input.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/jsx-props-no-spreading */
+import { TextInput, TextInputProps, View, ViewStyle } from 'react-native';
+import React from 'react';
+
+interface IInputProps extends TextInputProps{
+    leftIcon?: React.JSX.Element
+    rightIcon?: React.JSX.Element
+    containerStyle?:ViewStyle
+}
+
+export default function Input(props : IInputProps) {
+  const {
+    leftIcon,
+    rightIcon,
+    containerStyle,
+    style,
+  } = props;
+  return (
+    <View style={[{
+      borderWidth: 1,
+      flexDirection: 'row',
+      borderRadius: 7,
+    }, containerStyle]}
+    >
+      {leftIcon && leftIcon}
+      <TextInput
+        {...props}
+        style={[{
+          flex: 1,
+          paddingHorizontal: 10,
+          paddingVertical: 5,
+          fontSize: 16,
+        }, style]}
+      />
+      {rightIcon && rightIcon}
+    </View>
+  );
+}
