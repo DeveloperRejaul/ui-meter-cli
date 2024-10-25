e2ccdaba2e98159096ab13197d95c774b21e16e7
Rezaul Karim <rezaul@orangetoolz.com>
2024-10-25 22:07:37 +0600

    checkbox and input edite

--- src/content.ts
+++ src/content.ts
@@ -719,64 +719,73 @@ export function VStack(props: IVStack) {
 export function checkBoxContent() {
 
   return `
-import React, { useState } from "react";
-import { Pressable, Text, TextStyle, ViewProps, ViewStyle } from "react-native";
-
+import React, { useState } from 'react';
+import { Pressable, Text, TextStyle, ViewProps, ViewStyle } from 'react-native';
 
 interface ICheckBox extends ViewProps {
-    onChange?: (value: boolean) => void;
+    onChange?: (value: string | number) => void;
     defaultIsChecked?: boolean;
     isDisabled?: boolean
     borderColor?: string
     size?: number
     checkIcon?: React.JSX.Element
     boxStyle?: ViewStyle
+    boxActiveStyle?: ViewStyle
+    boxInactiveStyle?: ViewStyle
     checkStyle?: TextStyle
+    checkActiveStyle?: TextStyle
+    checkInactiveStyle?: TextStyle
     boxStyleClassName?: string;
     checkStyleClassName?: string;
+    value: string | number
 }
-
 
 export function CheckBox(props: ICheckBox) {
-    const {
-        defaultIsChecked = false,
-        isDisabled = false,
-        borderColor = "black",
-        size = 20,
-        checkIcon,
-        checkStyle,
-        boxStyle,
-        boxStyleClassName,
-        checkStyleClassName,
-        onChange,
-    } = props;
+  const {
+    defaultIsChecked = false,
+    isDisabled = false,
+    borderColor = 'black',
+    size = 20,
+    checkIcon,
+    checkStyle,
+    boxStyle,
+    boxActiveStyle,
+    boxInactiveStyle,
+    boxStyleClassName,
+    checkStyleClassName,
+    checkActiveStyle,
+    checkInactiveStyle,
+    onChange,
+    value,
+  } = props;
 
-    const [select, setSelect] = useState<boolean>(defaultIsChecked)
+  const [select, setSelect] = useState<boolean>(defaultIsChecked);
 
-    return (
-        <Pressable
-            pointerEvents={isDisabled ? "none" : "auto"}
-            style={{
-                borderWidth: 1,
-                borderColor,
-                height: size,
-                width: size,
-                justifyContent: "center",
-                alignItems: "center",
-                borderRadius: 3,
-                opacity: isDisabled ? 0.5 : 1,
-                ...boxStyle
-            }}
-            onPress={() => setSelect(pre => {
-                onChange?.(!pre)
-                return !pre
-            })}
-            {...{ className: boxStyleClassName }}
-        >
-            {select ? checkIcon ? checkIcon : <Text style={checkStyle} {...{ className: checkStyleClassName }}>✓</Text> : <></>}
-        </Pressable>
-    )
+  return (
+    <Pressable
+      pointerEvents={isDisabled ? 'none' : 'auto'}
+      style={[{
+        borderWidth: 1,
+        borderColor,
+        height: size,
+        width: size,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 3,
+        opacity: isDisabled ? 0.5 : 1,
+        ...boxStyle,
+      }, select ? boxActiveStyle : boxInactiveStyle]}
+      onPress={() => setSelect((pre) => {
+        onChange?.(value);
+        return !pre;
+      })}
+      {...{ className: boxStyleClassName }}
+    >
+      {select ? checkIcon || <Text style={[checkStyle, select ? checkActiveStyle : checkInactiveStyle]} {...{ className: checkStyleClassName }}>✓</Text> : null}
+    </Pressable>
+  );
 }
+
   `
 }
 
@@ -966,7 +975,7 @@ export default function AnimatedInput(props : IInputProps) {
     viewRef.current?.setNativeProps({ borderWidth: 0.8 });
     Animated.timing(animated, {
       toValue: 0.5,
-      useNativeDriver: true,
+      useNativeDriver: false,
       duration: 200,
     }).start();
   };
@@ -975,7 +984,7 @@ export default function AnimatedInput(props : IInputProps) {
     viewRef.current?.setNativeProps({ borderWidth: 1 });
     Animated.timing(animated, {
       toValue: 1,
-      useNativeDriver: true,
+      useNativeDriver: false,
       duration: 200,
     }).start();
   };
