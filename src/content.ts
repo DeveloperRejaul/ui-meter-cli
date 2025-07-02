d034c7407c49ed9dbff9fda903d2c449eb42f7ac
Rezaul Karim <rezaul@orangetoolz.com>
2025-07-02 18:32:34 +0600

    add

--- src/content.ts
+++ src/content.ts
@@ -1941,88 +1941,75 @@ export function getPieChartIndexCode() {
    *   BASE_STROKE_WIDTH={25}
    * />
    */
-  export default function PieChartIndex(props: PieChartProps) {
-    const {
-      data,
-      textTitle = "Hello World",
-      textValue = "257625",
-      GAP = 0,
-      SIZE = 300,
-      BASE_STROKE_WIDTH = 40
-    } = props;
+export default function PieChartIndex(props: PieChartProps) {
+  const {
+    data,
+    textTitle = "hello world",
+    textValue = "257625",
+    GAP = 0,
+    SIZE = 300,
+    BASE_STROKE_WIDTH = 40
+  } = props
+  const ADJUSTED_SIZE = SIZE + BASE_STROKE_WIDTH * 2
+  const CENTER = ADJUSTED_SIZE / 2
+  const RADIUS = SIZE / 2;
 
-    const ADJUSTED_SIZE = SIZE + BASE_STROKE_WIDTH * 2;
-    const CENTER = ADJUSTED_SIZE / 2;
-    const RADIUS = SIZE / 2;
+  const pieAnimation = useSharedValue(0);
+  const selectedSlice = useSharedValue<number | null>(null);
+  const fontSize = 17;
+  const font1 = useFont(require("./Roboto_Condensed-Regular.ttf"), fontSize);
+  const font2 = useFont(require("./Roboto-Bold.ttf"), fontSize);
 
-    const pieAnimation = useSharedValue(0);
-    const selectedSlice = useSharedValue<number | null>(null);
-    const fontSize = 17;
-    const font1 = useFont(require("./Roboto_Condensed-Regular.ttf"), fontSize);
-    const font2 = useFont(require("./Roboto-Bold.ttf"), fontSize);
+  useEffect(() => {
+    pieAnimation.value = 0;
+    pieAnimation.value = withTiming(1, { duration: 800 });
+  }, [data]);
 
-    useEffect(() => {
-      pieAnimation.value = 0;
-      pieAnimation.value = withTiming(1, { duration: 800 });
-    }, [data]);
+  const totalValue = useMemo(() => data.reduce((sum, item) => sum + item.value, 0),[data]);
+  const pieChartSlices = useMemo(() => {
+    let currentAngle = -90
 
-    const totalValue = useMemo(() => (
-      data.reduce((sum, item) => sum + item.value, 0)
-    ), [data]);
+    return data.map((item, index) => {
+      const proportion = item.value / totalValue;
+      const fullSweepAngle = proportion * 360;
 
-    const pieChartSlices = useMemo(() => {
-      let currentAngle = -90;
-      return data.map((item, index) => {
-        const proportion = item.value / totalValue;
-        const fullSweepAngle = proportion * 360;
-        const segmentStart = currentAngle;
-        currentAngle += fullSweepAngle;
+      const segmentStart = currentAngle;
+      currentAngle += fullSweepAngle;
 
-        return {
-          startAngle: segmentStart,
-          fullSweepAngle,
-          item,
-          index,
-          radius: RADIUS,
-          center: CENTER,
-          gap: GAP,
-          strokeWidth: BASE_STROKE_WIDTH,
-        };
-      });
-    }, [data]);
+      return {
+        startAngle: segmentStart,
+        fullSweepAngle,
+        item: item,
+        index: index,
+        radius: RADIUS,
+        center: CENTER,
+        gap: GAP,
+        strokeWidth: BASE_STROKE_WIDTH,
+      };
+    });
+  }, [data]);
 
-    return (
-      <Canvas style={{ width: ADJUSTED_SIZE, height: ADJUSTED_SIZE }}>
-        <Group>
-          <Text
-            x={CENTER}
-            y={CENTER - 10}
-            text={textTitle}
-            font={font1}
-            textAlign="center"
-            textBaseline="middle"
-          />
-          <Text
-            x={CENTER}
-            y={CENTER + 10}
-            text={textValue}
-            font={font2}
-            textAlign="center"
-            textBaseline="middle"
-          />
-          {pieChartSlices.map((slice, index) => (
-            <PieSlice
-              key={index}
-              {...slice}
-              index={index}
-              animatedValue={pieAnimation}
-              selectedSlice={selectedSlice}
+
+  return (
+        <Canvas style={{width: ADJUSTED_SIZE,height: ADJUSTED_SIZE}}>
+          <Group>
+            <Text
+                x={(CENTER - 3.2) - textTitle.length * 3.2}
+                y={CENTER - 10}
+                text={textTitle}
+                font={font1}  
             />
-          ))}
-        </Group>
-      </Canvas>
-    );
-  }
+            <Text
+                x={(CENTER - 4) - textValue.length * 4}
+                y={CENTER + 10}
+                text={textValue}
+                font={font2}  
+            />
+            {pieChartSlices.map((slice, index) =><PieSlice key={index}{...{...slice,index,animatedValue: pieAnimation,selectedSlice}}/>)}
+          </Group>
+        </Canvas>
+  );
+}
   `;
 }
 
